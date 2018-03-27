829eb6eaa859eba83a7666091f03afaa09972735
SebastianH11 <36234258+SebastianH11@users.noreply.github.com>
2018-03-26 23:04:49 -0400

    Add files via upload
    part 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,44 +23,82 @@ namespace u2SebastianTXTMSG
         public MainWindow()
         {
             InitializeComponent();
-            
+           
+
+
         }
 
         private void btnClick_Click(object sender, RoutedEventArgs e)
         {
-            
-            string TTYL = "TTYL";
             string Input = txtInput.Text;
-            //MessageBox.Show(Input);
-            string positionOfR = Input.Substring(Input.IndexOf("\r"));
-           //MessageBox.Show(positionOfR.ToString());
-            string Abreveation = Input.Substring(0, Input.IndexOf("\r"));
-            MessageBox.Show(Input.ToString());
-            
-            //MessageBox.Show(Abreveation);
-            //MessageBox.Show(Abreveation.Length.ToString());
-            while (Input != TTYL)
+            string Output = "";
+            string Line;
+            string LineEnding = "\r\n";
+            string LastLine = "TTYL";
+            int LineEndingLength = LineEnding.Length;
+            do
             {
-                if (Abreveation == "CU")
+                int EndIndex = Input.IndexOf(LineEnding);
+                if (EndIndex == -1)
                 {
-                    lblOutput.Content = "see you";
-                    
+                    EndIndex = Input.Length;
                 }
-                if (Abreveation == ":-)")
+                Line = Input.Substring(0, EndIndex);
+                if (Line == "CU")
                 {
-                    lblOutput.Content = "I'm Happy";
-                    MessageBox.Show("I'm Happy");
+                    Output = Output + "See you" + LineEnding;
                 }
-                if (Abreveation == ":-(")
+                else if (Line == ":-)")
                 {
-                    lblOutput.Content = "I'm Sad";
-                    MessageBox.Show("I'm Sad");
+                    Output = Output + "I'm happy" + LineEnding;
                 }
+                else if (Line == ":-(")
+                {
+                    Output = Output + "I'm Sad" + LineEnding;
+                }
+                else if (Line == ";-)")
+                {
+                    Output = Output + "Wink" + LineEnding;
+                }
+                else if (Line == ":-P")
+                {
+                    Output = Output + "stick out my toungue" + LineEnding;
+                }
+                else if (Line == "(~.~)")
+                {
+                    Output = Output + "sleepy" + LineEnding;
+                }
+                else if (Line == "TA")
+                {
+                    Output = Output + "totally awesome" + LineEnding;
+                }
+                else if (Line == "CCC")
+                {
+                    Output = Output + "Canadian Computing Competition" + LineEnding;
+                }
+                else if (Line == "CUZ")
+                {
+                    Output = Output + "thank-you" + LineEnding;
+                }
+                else if (Line == "YW")
+                {
+                    Output = Output + "you're welcome" + LineEnding;
+                }
+                else if (Line == LastLine)
+                {
+                    Output = Output + "Talk to you later";
+                }
+
                 else
                 {
-                   // MessageBox.Show("no");
+                    Output = Output + Line + LineEnding;
                 }
-            }
+                if (Line != LastLine)
+                {
+                    Input = Input.Substring(EndIndex + LineEndingLength, Input.Length - EndIndex - LineEndingLength);
+                }
+            } while (Line != LastLine);
+            lblOutput.Content = Output;
         }
     }
 }
